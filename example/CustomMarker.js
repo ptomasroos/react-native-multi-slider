c2b2b18ddf3936e02a10b4f0f664bd71d6811f76
Tomas Roos <ptomasroos@gmail.com>
2016-07-01 16:05:02 +0200

    Added example/

---
+++ example/CustomMarker.js
@@ -0,0 +1,27 @@
+'use strict';
+
+import React from 'react';
+
+import {
+  StyleSheet,
+  Image
+} from 'react-native';
+
+export default class CustomMarker extends React.Component {
+  render() {
+    return (
+      <Image
+        style={styles.image}
+        source={this.props.pressed ? require('./ruby.png') : require('./diamond.png')}
+        resizeMode='contain'
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  image: {
+    height: 40,
+    width: 40
+  }
+});
