be1e90a48e7bd8838fe9797b0e43bfcad2d29a8b
Elie Teyssedou <elie@deliver.ee>
2018-04-19 17:19:14 +0200

    Improve closest function

--- converters.js
+++ converters.js
@@ -1,5 +1,42 @@
 // Find closest index for a given value
-const closest = (array, n) => array.findIndex((elem) => (n <= elem));
+const closest = (array, n) => {
+  let minI = 0;
+  let maxI = array.length - 1;
+
+  if (array[minI] > n) {
+    return minI;
+  } else if (array[maxI] < n) {
+    return maxI;
+  } else if (array[minI] <= n && n <= array[maxI]) {
+    let closestIndex = null;
+
+    while (closestIndex === null) {
+      const midI = Math.round((minI + maxI) / 2);
+      const midVal = array[midI];
+
+      if (midVal === n) {
+        closestIndex = midI;
+      } else if (maxI === minI + 1) {
+        const minValue = array[minI];
+        const maxValue = array[maxI];
+        const deltaMin = Math.abs(minValue - n);
+        const deltaMax = Math.abs(maxValue - n);
+
+        closestIndex = deltaMax <= deltaMin ? maxI : minI;
+      } else if (midVal < n) {
+        minI = midI;
+      } else if (midVal > n) {
+        maxI = midI;
+      } else {
+        closestIndex = -1;
+      }
+    }
+
+    return closestIndex;
+  }
+
+  return -1;
+};
 
 export function valueToPosition(value, valuesArray, sliderLength) {
   const index = closest(valuesArray, value);
