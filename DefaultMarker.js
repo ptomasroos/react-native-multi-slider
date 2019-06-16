adef9bbf4d2ae9f2ff6cba3d6c0c1aa6eaa87ad4
hossein mohammadi <hosseinm.developer@gmail.com>
2019-06-16 10:41:28 +0430

    improve styles for support web (react-native-web) (#126)
    * improve styles for web
    
    * update version
    
    * Revert "update version"
    
    This reverts commit 67e4e4311be622cdbcbdcfcb3e379238830162ba.

--- DefaultMarker.js
+++ DefaultMarker.js
@@ -46,10 +46,26 @@ const styles = StyleSheet.create({
         borderRadius: 12,
         backgroundColor: '#0D8675',
       },
+      web: {
+        height: 30,
+        width: 30,
+        borderRadius: 30,
+        borderWidth: 1,
+        borderColor: '#DDDDDD',
+        backgroundColor: '#FFFFFF',
+        shadowColor: '#000000',
+        shadowOffset: {
+          width: 0,
+          height: 3,
+        },
+        shadowRadius: 1,
+        shadowOpacity: 0.2,
+      },
     }),
   },
   pressedMarkerStyle: {
     ...Platform.select({
+      web: {},
       ios: {},
       android: {
         height: 20,
